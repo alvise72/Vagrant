8f06674e9b10c31f7bb4eb50d724beea4a1a30a3
dorigo_a <alvise.dorigo@psi.ch>
2019-10-08 17:15:55 +0200

    fix

--- files/puppet/puppetsite/site.pp
+++ files/puppet/puppetsite/site.pp
@@ -1,29 +1,9 @@
 node default {
 }
 
-node 'linux1.psi.ch' {
-  class { 'gpfs::installrepo': }
-  class { 'gpfs::installrpms': }
-}
-
-node 'linux2.psi.ch' {
-class { 'gpfs::installrepo': }
-  class { 'gpfs::installrpms': }
-include ::lvm
-}
-
-node 'linux3.psi.ch' {
-class { 'gpfs::installrepo': }
-  class { 'gpfs::installrpms': }
-include ::lvm
-}
-node 'linux4.psi.ch' {
-class { 'gpfs::installrepo': }
-  class { 'gpfs::installrpms': }
-include ::lvm
-}
-node 'linux5.psi.ch' {
-class { 'gpfs::installrepo': }
-  class { 'gpfs::installrpms': }
-include ::lvm
+node 'elk.psi.ch' {
+  class { 'elk::installrepo': }
+  class { 'elk::installrpms': }
+  class { 'elk::startservices': }
+  include ::lvm
 }
