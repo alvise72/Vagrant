bc8b1a09efdae75fedbb71b29d25ca3d18b711d0
dorigo_a <alvise.dorigo@psi.ch>
2019-10-08 17:38:43 +0200

    fixe

--- files/puppet/puppetsite/site.pp
+++ files/puppet/puppetsite/site.pp
@@ -4,6 +4,7 @@ node default {
 node 'elk.psi.ch' {
   class { 'elk::installrepo': }
   class { 'elk::installrpms': }
+  class { 'elk::configurepaths': }
   class { 'elk::startservices': }
   include ::lvm
 }
