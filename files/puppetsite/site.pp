6550d28df3a65c7d6ceee513f92af32ce3398631
Dorigo Alvise <dorigo_a@sf-cn-16.psi.ch>
2019-10-08 11:30:09 +0200

    fix

--- files/puppetsite/site.pp
+++ files/puppetsite/site.pp
@@ -1,22 +1,29 @@
 node default {
+}
+
+node 'linux1.psi.ch' {
   class { 'gpfs::installrepo': }
   class { 'gpfs::installrpms': }
 }
 
-node 'linux1.psi.ch' {
-}
-
 node 'linux2.psi.ch' {
+class { 'gpfs::installrepo': }
+  class { 'gpfs::installrpms': }
 include ::lvm
 }
 
 node 'linux3.psi.ch' {
+class { 'gpfs::installrepo': }
+  class { 'gpfs::installrpms': }
 include ::lvm
 }
 node 'linux4.psi.ch' {
+class { 'gpfs::installrepo': }
+  class { 'gpfs::installrpms': }
 include ::lvm
 }
 node 'linux5.psi.ch' {
+class { 'gpfs::installrepo': }
+  class { 'gpfs::installrpms': }
 include ::lvm
 }
-
--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-egrep 'linux[1-7]+' Vagrantfile|awk '{print $1}'|sed 's+\"++g'| xargs -P0 -I {} vagrant --storage-path=/gpfs/photonics/swissfel/res/dorigo_a/vbox --cores=1 --memory=1024 --disk-size=20 up {}
+seq 1 7| xargs -n1 -P7 -I{} vagrant --storage-path=/gpfs/photonics/swissfel/res/dorigo_a/vbox --cores=4 --memory=8192 up linux{}
