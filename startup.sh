4e75995e093b3a7df1083c83963bfb27bc6d3598
Alvise Dorigo <dorigo_a@Mac-mini-di-Alvise.local>
2020-01-11 10:16:36 +0100

    fixes

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 seq 1 7| xargs -n1 -P7 -I{} vagrant --storage-path=/gpfs/photonics/swissfel/res/dorigo_a/vbox --cores=4 --memory=8192 up linux{}
+
+# Startup ansible nodes @Home
+# vagrant --storage-path=/Volumes/Home/Virtual\ Machines.localized --cores=1 --memory=2048 up control
+# vagrant --storage-path=/Volumes/Home/Virtual\ Machines.localized --cores=1 --memory=2048 up managed
