d9be58a62cc61a6a9c65fc98470dbd1b9c3c0146
Alvise Dorigo <dorigo_a@Mac-mini-di-Alvise.local>
2020-01-11 10:14:13 +0100

    fixes

--- scripts/setup-ansible-server.sh
+++ scripts/setup-ansible-server.sh
@@ -12,9 +12,10 @@ rsync -avz /vagrant/keys/$HOSTNAME/ /root/.ssh/
 cp /vagrant/keys/authorized_keys /root/.ssh/authorized_keys
 cp /vagrant/keys/known_hosts /root/.ssh/
 cp /vagrant/keys/vagrant_user/id_rsa* ~vagrant/.ssh/
-cp /vagrant/keys/vagrant_user/known_hosts ~vagrant/.ssh/
 chmod 0400 ~vagrant/.ssh/id_rsa
 cat ~vagrant/.ssh/id_rsa.pub >> ~vagrant/.ssh/authorized_keys
+touch ~vagrant/.ssh/config
+echo "StrictHostKeyChecking no" >>  ~vagrant/.ssh/config
 chown -R root:root /root/.ssh/
 chmod 700 /root/.ssh/
 chmod 0400 /root/.ssh/id_dsa
