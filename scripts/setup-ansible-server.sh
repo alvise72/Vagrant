94770d6d17a0c07128198bb9d8b3821822e26013
dorigo_a <alvise.dorigo@psi.ch>
2020-01-15 14:01:25 +0100

    fixes

--- scripts/setup-ansible-server.sh
+++ scripts/setup-ansible-server.sh
@@ -1,4 +1,5 @@
 yum install -y epel-release rsync lvm2 gcc-c++ make cmake net-tools sysstat dstat git epel-release chrony telnet mlocate lsof bind-utils vim
+yum install -y python2 ansible
 
 systemctl disable firewalld
 systemctl start chronyd
@@ -26,8 +27,6 @@ cat /vagrant/hosts >> /etc/hosts
 
 cp /vagrant/files/control/etc/ssh/ssh_config.d/nohostcheck.conf /etc/ssh/ssh_config.d/nohostcheck.conf
 
-yum -y install ansible python2
-
 useradd dorigo_a
 mkdir ~dorigo_a/.ssh
 chmod 0755 ~dorigo_a/.ssh
@@ -37,8 +36,15 @@ cp /vagrant/files/dorigo_a/ssh_config ~dorigo_a/.ssh/config
 cp /vagrant/files/dorigo_a/vpn-g.sh /usr/local/bin
 chmod 755 /usr/local/bin/vpn-g.sh
 mkdir ~dorigo_a/.ssh/tmp
-chown -R dorigo_a:dorigo_a ~dorigo_a
 cp /vagrant/files/dorigo_a/dorigo_a-sudoer /etc/sudoers.d/dorigo_a
+mkdir -p ~/dorigo_a/ansible
+cp /vagrant/files/dorigo_a/ansible.cfg /vagrant/files/dorigo_a/hosts ~/dorigo_a/ansible/
 yum install -y https://rpmfind.net/linux/epel/7/x86_64/Packages/s/sshuttle-0-8.20120810git9ce2fa0.el7.noarch.rpm
 ln -s /usr/bin/python2 /usr/bin/python
+pip-3.6 install python-hpilo python-ilorest-library
+
+chown -R dorigo_a:dorigo_a ~dorigo_a
+
+echo "sudo su - dorigo_a" >> ~vagrant/.bash_profile
+
 reboot
