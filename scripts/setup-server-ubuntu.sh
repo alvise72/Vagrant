725b85509f35d2eedcd6c098fa3e629234e51b41
Dorigo Alvise <dorigo_a@sf-cn-16.psi.ch>
2020-03-13 09:49:05 +0100

    fixes

---
+++ scripts/setup-server-ubuntu.sh
@@ -0,0 +1,25 @@
+apt-get install -y rsync lvm2 gcc-c++ make cmake net-tools sysstat dstat git epel-release chrony telnet mlocate lsof bind-utils vim
+
+systemctl disable firewalld
+systemctl start chronyd
+systemctl enable chronyd
+
+sed -i 's+^SELINUX=.*+SELINUX=disabled+' /etc/selinux/config
+
+#yum -y update
+apt-get update -y
+mkdir -p /root/.ssh/
+rsync -avz /vagrant/keys/$HOSTNAME/ /root/.ssh/
+cp /vagrant/keys/authorized_keys /root/.ssh/authorized_keys
+cp /vagrant/keys/known_hosts /root/.ssh/
+cp /vagrant/keys/vagrant_user/id_rsa* ~vagrant/.ssh/
+chmod 0400 ~vagrant/.ssh/id_rsa
+cat ~vagrant/.ssh/id_rsa.pub >> ~vagrant/.ssh/authorized_keys
+
+chown -R root:root /root/.ssh/
+chmod 700 /root/.ssh/
+chmod 0400 /root/.ssh/id_dsa
+
+cat /vagrant/hosts >> /etc/hosts
+
+reboot
