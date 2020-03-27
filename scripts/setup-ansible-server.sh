8e07b8ef13c272750fc97f94416b3bc46c234dfa
Dorigo Alvise <dorigo_a@sf-cn-16.psi.ch>
2020-03-28 00:48:59 +0100

    fix

--- scripts/setup-ansible-server.sh
+++ scripts/setup-ansible-server.sh
@@ -1,6 +1,6 @@
 yum install -y epel-release
 yum clean all
-yum -y install rsync lvm2 gcc-c++ make cmake net-tools sysstat dstat git epel-release chrony telnet mlocate lsof bind-utils vim python2 ansible strace tree
+yum -y install rsync lvm2 gcc-c++ make cmake net-tools sysstat dstat git epel-release chrony telnet mlocate lsof bind-utils vim python2 ansible strace tree rhel-system-roles
 
 systemctl disable firewalld
 systemctl start chronyd
