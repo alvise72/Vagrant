a502ddbc1eaa61107d727bc1a1fee77efbf1d7b6
Dorigo Alvise <dorigo_a@sf-cn-16.psi.ch>
2020-05-07 16:13:19 +0200

    fixes

--- scripts/setup-ansible-server.sh
+++ scripts/setup-ansible-server.sh
@@ -10,31 +10,24 @@ sed -i 's+^SELINUX=.*+SELINUX=disabled+' /etc/selinux/config
 
 yum -y update
 mkdir -p /root/.ssh/
-rsync -avz /vagrant/keys/$HOSTNAME/ /root/.ssh/
-#mkdir ~vagrant/local/
-#chown vagrant:vagrant ~vagrant/local/
-#rsync -avz /vagrant/files/local ~vagrant/local/
-cp /vagrant/keys/authorized_keys /root/.ssh/authorized_keys
-cp /vagrant/keys/known_hosts /root/.ssh/
-cp /vagrant/keys/vagrant_user/id_rsa* ~vagrant/.ssh/
-chmod 0400 ~vagrant/.ssh/id_rsa
-cat ~vagrant/.ssh/id_rsa.pub >> ~vagrant/.ssh/authorized_keys
-touch ~vagrant/.ssh/config
-#echo "StrictHostKeyChecking no" >>  ~vagrant/.ssh/config
-chown -R root:root /root/.ssh/
-chmod 700 /root/.ssh/
+rsync -avz /vagrant/keys/root_user/ /root/.ssh/
 chmod 0400 /root/.ssh/id_dsa
+chmod 0600 /root/.ssh/authorized_keys
+#cat ~vagrant/.ssh/id_rsa.pub >> ~vagrant/.ssh/authorized_keys
+#touch ~vagrant/.ssh/config
+#echo "StrictHostKeyChecking no" >>  ~vagrant/.ssh/config
+#chown -R root:root /root/.ssh/
+chmod 700 /root/.ssh/
+#chmod 0400 /root/.ssh/id_dsa
 
-chown -R vagrant:vagrant /home/vagrant
+#chown -R vagrant:vagrant /home/vagrant
 
 cat /vagrant/hosts >> /etc/hosts
 
 cp /vagrant/files/control/etc/ssh/ssh_config.d/nohostcheck.conf /etc/ssh/ssh_config.d/nohostcheck.conf
 
-#cp /vagrant/files/dorigo_a/ssh_config ~dorigo_a/.ssh/config
-#cp /vagrant/files/dorigo_a/vpn-g.sh /usr/local/bin
-cp -r /vagrant/files/control/ansible ~vagrant
-chown -R vagrant ~vagrant/*
+#cp -r /vagrant/files/control/ansible ~vagrant
+#chown -R vagrant ~vagrant/*
 
 #yum install -y https://rpmfind.net/linux/epel/7/x86_64/Packages/s/sshuttle-0-8.20120810git9ce2fa0.el7.noarch.rpm
 #ln -s /usr/bin/python2 /usr/bin/python
